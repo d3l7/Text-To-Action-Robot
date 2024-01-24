0711f85ae5d7f1e367ab79c9886dbe8ce5e65efa
d3l7 <marcussp1310@gmail.com>
2024-01-24 14:23:13 +0000

    aaaa

--- thresholding.py
+++ thresholding.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread('sudoku.jpg', cv.IMREAD_GRAYSCALE)
+img = cv.imread('download.jpg', cv.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read, check with os.path.exists()"
 img = cv.medianBlur(img,5)
 ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
