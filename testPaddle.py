90ab2fe29409978398c77e29806fa90ed2f2b492
joelmurray0 <144688121+joelmurray0@users.noreply.github.com>
2024-02-22 20:59:27 +0000

    paddle work

---
+++ testPaddle.py
@@ -0,0 +1,11 @@
+from paddleocr import PaddleOCR
+# Paddleocr supports Chinese, English, French, German, Korean and Japanese.
+# You can set the parameter `lang` as `ch`, `en`, `french`, `german`, `korean`, `japan`
+# to switch the language model in order.
+ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
+img_path = 'Example-image-of-a-general-handwritten-text-paragraph-from-IAM-dataset-4.png'
+result = ocr.ocr(img_path, cls=True)
+for idx in range(len(result)):
+    res = result[idx]
+    for line in res:
+        print(line)
